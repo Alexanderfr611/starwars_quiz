1205e4681e11176889b70458f20ea39e7a48b926
alexander <alex2000.fritz@outlook.de>
2023-02-22 18:27:40 +0100

    Instruction how the quiz is working

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Herzlich Willkommen bei dem Star Wars Quiz \n" +
+      "Du erhälst nun 10 Fragen zu dem Star Wars Universum, am Ende siehst du dann dein Ergebnis!. \n" +
+      "Zuerst beginnen wir mit ein paar Grundlegenden Sachen.")
+
+name_eingabe = input("Wie ist dein Name?")
+print(name_eingabe.lstrip() + " ist ein sehr schöner Name")
+
+alter_eingabe = input("Wie alt bist du?")
+
+
+
+
