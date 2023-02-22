bb75d3b00313230ad2992265a91f4179eae8c8ad
BuildTools <unconfigured@null.spigotmc.org>
2023-02-22 20:00:49 +0100

    Creating the starting gui

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-print("Herzlich Willkommen bei dem Star Wars Quiz \n" +
-      "Du erhälst nun 10 Fragen zu dem Star Wars Universum, am Ende siehst du dann dein Ergebnis!. \n" +
-      "Zuerst beginnen wir mit ein paar Grundlegenden Sachen.")
+import tkinter as gui
 
-name_eingabe = input("Wie ist dein Name?")
-print(name_eingabe.lstrip() + " ist ein sehr schöner Name")
+# Creating a Gui where you can do the quiz
+window = gui.Tk()
+greeting = gui.Label(text="Herzlich Willkommen bei dem Star Wars Quiz \n" +
+    "Du erhältst nun 10 Fragen zu dem Star Wars Universum, am Ende siehst du wie Mächtig du wirklich bist. \n" +
+    "Zuerst beginnen wir mit ein paar Grundlegenden Sachen.",
+    bg="black",
+    fg="red",
+    width=100,
+    height=25,
+    font=("Arial", 14)
+)
+greeting.pack()
+window.mainloop()
 
-alter_eingabe = input("Wie alt bist du?")
 
 
 
